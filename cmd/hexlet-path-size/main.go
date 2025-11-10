181c9e214114c13240f90507c5d1317a3cf7caba
Pavel <voroshilov.pavel@gmail.com>
2025-11-10 10:09:37 +0300

    feat: func getSize

--- cmd/hexlet-path-size/main.go
+++ cmd/hexlet-path-size/main.go
@@ -2,30 +2,29 @@ package main
 
 import (
 	si "code/sizeinformer"
-	"context"
 	"fmt"
-	"log"
-	"os"
-
-	"github.com/urfave/cli/v3"
 )
 
 func main() {
+	/*
+		cmd := &cli.Command{
+			Name:  "hexlet-path-size",
+			Usage: "print size of a file or directory",
+			Action: func(context.Context, *cli.Command) error {
+				fmt.Println("Hello friend!")
+				return nil
+			},
+		}
 
-	cmd := &cli.Command{
-		Name:  "hexlet-path-size",
-		Usage: "print size of a file or directory",
-		Action: func(context.Context, *cli.Command) error {
-			fmt.Println("Hello friend!")
-			return nil
-		},
+		if err := cmd.Run(context.Background(), os.Args); err != nil {
+			log.Fatal(err)
+		}
+	*/
+	size, err := si.GetSize("/tmp/dir200")
+	//size, err := si.GetSize("/tmp/file2.pdf")
+	if err != nil {
+		fmt.Println(err)
 	}
-
-	if err := cmd.Run(context.Background(), os.Args); err != nil {
-		log.Fatal(err)
-	}
-
-	size := si.GetSize("/tmp/test2.txt")
 	fmt.Println(size)
 
 }
--- sizeinformer/sizeinformer.go
+++ sizeinformer/sizeinformer.go
@@ -2,15 +2,68 @@ package sizeinformer
 
 import (
 	"fmt"
+	"io/fs"
 	"os"
+	"path/filepath"
 )
 
-func GetSize(path string) int {
+func GetSize(path string) (string, error) {
 	file, err := os.Lstat(path)
 	if err != nil {
-		fmt.Printf("Невозможно открыть файл : %q", path)
+		return "", fmt.Errorf("Невозможно открыть файл : %q", path)
 	}
 
-	return int(file.Size())
+	if !file.IsDir() {
+		value := formatFileSize(file.Size())
+		return fmt.Sprintf("%s	%s\n", value, path), nil
+	}
+
+	dirSize, err := getDirSize(path)
+	if err != nil {
+		return "", fmt.Errorf("Ошибка обхода директории : %q", path)
+	}
+	dirSizeInfo := formatFileSize(dirSize)
+	return fmt.Sprintf("%s	%s\n", dirSizeInfo, path), nil
+
+}
+
+func formatFileSize(size int64) string {
+
+	const (
+		KB = 1024
+		MB = KB * 1024
+		GB = MB * 1024
+	)
+
+	switch {
+	case size > GB:
+		return fmt.Sprintf("%0.fGB", float64(size)/float64(GB))
+	case size > MB:
+		return fmt.Sprintf("%0.fMB", float64(size)/float64(MB))
+	case size > KB:
+		return fmt.Sprintf("%0.fKB", float64(size)/float64(KB))
+	default:
+		return fmt.Sprintf("%dB", size)
+	}
+}
+
+func getDirSize(path string) (int64, error) {
+
+	var totalSize int64
+	err := filepath.Walk(path, func(filePath string, info fs.FileInfo, err error) error {
+
+		if err != nil {
+			return err
+		}
+
+		if !info.IsDir() {
+			totalSize += info.Size()
+		}
+
+		return nil
+
+	})
+
+	return totalSize, err
 
 }
