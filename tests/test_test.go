518c0a2a6884a69d88fc6bcf9405995680a76709
Pavel <voroshilov.pavel@gmail.com>
2025-11-12 09:40:24 +0300

    fix test3

--- tests/test_test.go
+++ tests/test_test.go
@@ -1,12 +1,18 @@
 package tests
 
 import (
+	pathsize "code/path_size"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestTeat(t *testing.T) {
+	// Пример использования твоего пакета
+	result, err := pathsize.GetSize(".", false, false, false)
+	assert.NoError(t, err)
+	assert.NotEmpty(t, result)
+
 	b := 10
 	assert.Equal(t, b, 10)
 }
